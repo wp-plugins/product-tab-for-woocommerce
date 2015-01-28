2e808927cd34d74e3cec6c57a5fca6e388e64812
jigneshkailam <jigneshkailam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 02:52:12 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/product-tab-for-woocommerce/trunk@1077087 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-product-tab-for-woocommerce-deactivator.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @class       MBJ_Product_Tab_For_WooCommerce_Deactivator
+ * @version	1.0.0
+ * @package	product-tab-for-woocommerce
+ * @category	Class
+ * @author      jigneshkailam <phpwebcreators@gmail.com>
+ */
+class MBJ_Product_Tab_For_WooCommerce_Deactivator {
+
+    /**
+     * @since    1.0.0
+     */
+    public static function deactivate() {
+        
+    }
+
+}
