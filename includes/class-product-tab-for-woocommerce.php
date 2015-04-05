7bc85fa268f94403bcd757460e0d43c2533b1bf2
jigneshkailam <jigneshkailam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-05 15:52:49 +0000

    change version
    git-svn-id: https://plugins.svn.wordpress.org/product-tab-for-woocommerce/trunk@1128236 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-product-tab-for-woocommerce.php
+++ includes/class-product-tab-for-woocommerce.php
@@ -49,7 +49,7 @@ class MBJ_Product_Tab_For_WooCommerce {
     public function __construct() {
 
         $this->plugin_name = 'Product Tab For WooCommerce';
-        $this->version = '1.0.3';
+        $this->version = '1.0.4';
 
         $this->load_dependencies();
         $this->set_locale();
